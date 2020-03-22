007b3b508574a4805143be18ab229465094ef63c
ShimizuShiori <116971263@qq.com>
2020-03-22 18:57:56 +0800

    添加新项目，与 autofac 的集成

---
+++ src/Reface.CommandBus.IntegrateAutofac/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reface.CommandBus.IntegrateAutofac
+{
+    public class Class1
+    {
+    }
+}
